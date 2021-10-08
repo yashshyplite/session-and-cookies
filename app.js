7594795583a167ea5b4aa20b8897a61225574822
yash <yashgupta@shyplite.com>
2021-10-08 11:00:35 +0530

    sentry

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require("express")
 const session = require("express-session")
 const cookieParser = require('cookie-parser');
 const bodyParser= require('body-parser');
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 
-// Setting up the server
 var app = express();
 
+Sentry.init({
+    dsn: "https://e25d741c54c641f78f5cf6e17e8fb832@o1029468.ingest.sentry.io/5996959",
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    release: "session@" + process.env.npm_package_version,
+    tracesSampleRate: 1.0,
+    environment: "production",
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.errorHandler());
 app.use(bodyParser.urlencoded({extended:true}));
+
 // Creating session
 app.use(cookieParser());
 app.use(session({
@@ -22,7 +38,7 @@ app.use(session({
 
 var redirectLogin = (req,res,next)=>{
     if(!req.session.userID){
-        console.log('redirect to login')
+        // console.log('redirect to login')
         res.redirect('/login');
     }
     else{
@@ -32,7 +48,7 @@ var redirectLogin = (req,res,next)=>{
 
 var redirecthome = (req,res,next)=>{
     if(req.session.userID){
-        console.log('redirect to home')
+        // console.log('redirect to home')
         res.redirect('/');
     }
     else{
@@ -47,27 +63,33 @@ app.route('/')
 
 app.route('/login')
 .get(redirecthome,(req,res)=>{
+    Sentry.captureMessage("login");
     res.sendFile(__dirname+'/index.html')
 })
 .post(redirecthome,(req,res)=>{
     var un=req.body.Id;
     var p = req.body.Password;
-    console.log('logging in')
+    // console.log('logging in')
     try{
         if(un=='xxx'&&p=='xxx'){
             req.session.userID=un;
             return res.redirect('/');
         }
+        else{
+            Sentry.captureException("wrong password");
+        }
         res.redirect('/login')
     }
     catch(err){
-        console.log(err);
+        Sentry.captureException(err);
     }
 });
+
+
 app.route('/logout')
 .post((req,res)=>{
     req.session.destroy(err=>{
-        console.log(err);
+        // console.log(err);
         if(err!=undefined)
         return res.redirect('/');
     })
